07c886063ae9bb494a06ade0f3819b816e6083cb
mirza rizky lutfianto <mirzalutfianto@yahoo.co.id>
2017-04-01 11:54:46 +0700

    Update Tugas2.php

--- Tugas2.php
+++ Tugas2.php
@@ -1,18 +1,50 @@
 <?php
-	$json_string = file_get_contents("http://api.wunderground.com/api/0e4b71198dc78edf/geolookup/conditions/q/IA/mugassari.json");
+	//proses get file json
+	$json_string = file_get_contents("http://api.wunderground.com/api/a657d7d2ba430b38/webcams/q/CA/San_Francisco.json");
 	$json_string2 = file_get_contents("http://api.wunderground.com/api/a657d7d2ba430b38/astronomy/q/IA/mugassari.json");
-	$json_string3 = file_get_contents ("http://api.wunderground.com/api/a657d7d2ba430b38/planner_07010731/q/IA/mugassari.json");
+	$json_string3 = file_get_contents ("http://api.wunderground.com/api/a657d7d2ba430b38/planner_08090831/q/IA/mugassari.json");
 	$parsed_json = json_decode ($json_string);
 	$parsed_json2 = json_decode ($json_string2);
 	$parsed_json3 = json_decode ($json_string3);
-	$location = $parsed_json->{'response'}->{'version'};
-	$test = $parsed_json2->{"moon_phase"}->{"phaseofMoon"};
-	$a = $parsed_json3->{"trip"}->{"title"};
-	echo "Di lihat dari Indonesia";
+	
+	//proses json
+	$a1 = $parsed_json->webcams[0]->{"camid"};
+	$a2 = $parsed_json->webcams[0]->{"updated"};
+	$a3 = $parsed_json->webcams[0]->{"city"};
+	
+	$b1 = $parsed_json2->{"moon_phase"}->{"phaseofMoon"};
+	$b2 = $parsed_json2->{"moon_phase"}->{"ageOfMoon"};
+	$b3 = $parsed_json2->{"moon_phase"}->{"percentIlluminated"};
+	
+	$c1 = $parsed_json3->{"trip"}->{"title"};
+	$c2 = $parsed_json3->{"trip"}->{"airport_code"};
+        $c3 = $parsed_json3->{"trip"}->{"period_of_record"}->{"date_start"}->{"date"}->{"pretty"};
+	
+	
+	//memanggil hasil proses json
+	echo "Tugas 2 menampilkan json";
 	echo "<br>";
-	echo "Current temperature in ${location}\n";
-    echo "<br>";	
-	echo "Fase bulan di indonesia melalui astronomy yaitu fase : ${test}\n";
+	echo "Camera type ${a1}\n";
 	echo "<br>";
-	echo "Jumlah hari di bulan July : ${a}\n";
+	echo "Update foto ${a2}\n";
+	echo "<br>";
+	echo "Didapat dari ${a3}\n";
+        echo "<br>";
+	echo "<br>";
+	echo "Element dari json astronomy";
+	echo "<br>";
+	echo "Fase Bulan ${b1}\n";
+	echo "<br>";
+	echo "Umur Bulan : ${b2}\n";
+	echo "<br>";
+	echo "Persentase Diterjang : ${b3}\n";
+	echo "<br>";
+	echo "<br>";
+	echo "Element dari json planner";
+	echo "<br>";
+	echo "Judul : ${c1}\n";
+	echo "<br>";
+	echo "Code Pesawat terbang dibulan ini : ${c2}\n";
+	echo "<br>";
+	echo "Tanggal cantik: ${c3}\n";
 ?>
